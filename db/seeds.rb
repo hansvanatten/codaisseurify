fb52cd4eb4d4c9e4bee1e9c3e39e3ba32e621ac8
hansvanatten <hansvanatten@gmail.com>
2017-01-15 22:26:03 +0100

    artist seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
 song1 = Song.create!({name: "Bohemian Rhapsody"})
 song2 = Song.create!({name: "Stressed Out"})
 song3 = Song.create!({name: "Ride"})
 song4 = Song.create!({name: "Hotel California"})
 song5 = Song.create!({name: "Californication"})
 song6 = Song.create!({name: "Dark Necessities"})
+
+artist1 = Artist.create!(name: "Queen")
+artist2 = Artist.create!(name: "Twenty One Pilots")
+artist3 = Artist.create!(name: "Eagles")
+artist4 = Artist.create!(name: "Red Hot Chili Peppers")
+
+artist1.songs << [song1]
+artist1.save
+artist2.songs << [song2, song3]
+artist2.save
+artist3.songs << [song4]
+artist3.save
+artist4.songs << [song5, song6]
+artist4.save

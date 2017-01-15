1ad2b91e414454c661197c9f2d5fca6fe013cfdd
hansvanatten <hansvanatten@gmail.com>
2017-01-15 21:57:35 +0100

    create artist model and associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170115172118) do
+ActiveRecord::Schema.define(version: 20170115205250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "songs", force: :cascade do |t|
+  create_table "artists", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "songs", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "artist_id"
+    t.index ["artist_id"], name: "index_songs_on_artist_id", using: :btree
+  end
+
+  add_foreign_key "songs", "artists"
 end

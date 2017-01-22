94992dbd46da0f4f186c81ccf52692a4cb14af0a
hansvanatten <hansvanatten@gmail.com>
2017-01-22 18:04:37 +0100

    delete artist

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,4 +2,13 @@ class ArtistsController < ApplicationController
   def index
     @artists = Artist.all
   end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def delete
+    @artist.destroy
+    redirect_to artists_path
+  end
 end

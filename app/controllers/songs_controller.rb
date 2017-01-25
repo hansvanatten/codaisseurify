cec05332f4823e08484d8a468738b6cfd45df785
hansvanatten <hansvanatten@gmail.com>
2017-01-25 20:45:27 +0100

    fixed add song-delete artist

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,16 +3,16 @@ class SongsController < ApplicationController
   def create
     @song = @artist.songs.create(song_params)
     if @song.save
-      redirect_to artist_path(@artist), :notice "Song successfully added"
+      redirect_to artist_path(@artist), notice: "Song successfully added"
     else
-      redirect_to artist_path(@artist), :notice "Failed to add song"
+      redirect_to artist_path(@artist), notice: "Failed to add song"
     end
   end
 
   def destroy
     @song = Song.find(params[:id])
     @song.destroy
-    redirect_to artist_path)@artist), :notice "Song successfully deleted"
+    redirect_to artist_path(@artist), notice: "Song successfully deleted"
   end
 
   private
@@ -21,6 +21,6 @@ class SongsController < ApplicationController
   end
 
   def set_artist
-        @artist = Artist.find(params[:id])
+        @artist = Artist.find(params[:artist_id])
   end
 end

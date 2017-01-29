56c4fe4f595afc9e14270ae153116d0a24eecaf1
hansvanatten <hansvanatten@gmail.com>
2017-01-29 22:48:42 +0100

    songs controller respond

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,17 +2,32 @@ class SongsController < ApplicationController
   before_action :set_artist
   def create
     @song = @artist.songs.create(song_params)
-    if @song.save
-      redirect_to artist_path(@artist), notice: "Song successfully added"
-    else
-      redirect_to artist_path(@artist), notice: "Failed to add song"
+    # if @song.save
+    #   redirect_to artist_path(@artist), notice: "Song successfully added"
+    # else
+    #   redirect_to artist_path(@artist), notice: "Failed to add song"
+    # end
+    respond_to do |format|
+      if @song.save
+        format.html { redirect_to artist_path(@artist), notice: 'Song added successfully.' }
+        format.json { render :show, status: :created, location: @song }
+      else
+        format.html { redirect_to artist_path(@artist) }
+        format.json { render json: @song.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def destroy
     @song = Song.find(params[:id])
-    @song.destroy
-    redirect_to artist_path(@artist), notice: "Song successfully deleted"
+    respond_to do |format|
+
+      if @song.destroy
+      # redirect_to artist_path(@artist), notice: "Song successfully deleted"
+        format.html { redirect_to artist_path(@artist), notice: 'Song successfully deleted.' }
+        format.json { render :show, status: :destroyed, location: @song }
+      end
+    end
   end
 
   private
